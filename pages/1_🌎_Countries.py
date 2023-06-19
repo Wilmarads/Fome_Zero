7127da7da0326939bb4bf60e59b3ef840640a599
Wilmara Alves <125622237+Wilmarads@users.noreply.github.com>
2023-06-19 12:07:05 -0300

    Add files via upload

--- pages/1_🌎_Countries.py
+++ pages/1_🌎_Countries.py
@@ -287,7 +287,7 @@ st.sidebar.markdown( """---""" )
 
 st.sidebar.markdown( '# Filtros' )
 
-#country_options = st.sidebar.multiselect('Selecione o país que deseja visualizar o restaurante', ['India', 'Australia', 'Brazil', 'Canada', 'Indonesia', 'New Zeland', 'Philippines', 'Qatar', 'Singapure', 'South Africa', 'Sri Lanka', 'Turkey', 'United Arab Emirates', 'England', 'United States of America'], default=['India', 'Australia', 'Brazil', 'Canada', 'Indonesia', 'New Zeland', 'Philippines', 'Qatar', 'Singapure', 'South Africa', 'Sri Lanka', 'Turkey', 'United Arab Emirates', 'England', 'United States of America'])
+country_options = st.sidebar.multiselect('Selecione o país que deseja visualizar o restaurante', ['India', 'Australia', 'Brazil', 'Canada', 'Indonesia', 'New Zeland', 'Philippines', 'Qatar', 'Singapure', 'South Africa', 'Sri Lanka', 'Turkey', 'United Arab Emirates', 'England', 'United States of America'], default=['India', 'Australia', 'Brazil', 'Canada', 'Indonesia', 'New Zeland', 'Philippines', 'Qatar', 'Singapure', 'South Africa', 'Sri Lanka', 'Turkey', 'United Arab Emirates', 'England', 'United States of America'])
 
 
 
@@ -296,8 +296,8 @@ st.sidebar.markdown( """---""" )
 st.sidebar.markdown( '## Eat wherever you are!' )
 
 # Filtro de país
-#linhas_selecionadas = df["country_code"].isin( country_options )
-#df = df.loc[linhas_selecionadas, :]
+linhas_selecionadas = df["Country Code"].isin( country_options )
+df = df.loc[linhas_selecionadas, :]
 
 # =======================================
 # Layout no Streamlit
